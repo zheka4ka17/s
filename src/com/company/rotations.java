2c7211578aa9c04a9a7f752edcca96eb72c5ecac
гк <zheka4ka17@gmail.com>
2022-10-31 21:29:07 +0300

    message

--- src/com/company/rotations.java
+++ src/com/company/rotations.java
@@ -4,14 +4,28 @@ public class rotations {
     public static void main(String[] args) {
         String word = "abc";
         String[] rots = {"abc","bca", "cab"};
-        int length = word.length();
-        for (int i=0;i<length; i++){
-            boolean flag = false;
-            for (int j=0; j< rots.length; j++)
-                if (word.equals(rots[i]))
-                    flag= true;
-
+       int length=word.length();
+        String curr = word;
+        int count =0; //Счетчик поворотов
+        for (int i=0; i<length; i++){
+            boolean found = false;
+            for (int j=0; j<length; j++)
+                if (rots[j].equals(curr)) //Через = не получается
+                    found = true;
+            if (found) count++;
+            StringBuilder sb = new StringBuilder(curr);
+            char c = curr.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(c);
+            curr = sb.toString();
         }
+        System.out.println(count);
+        boolean result = count==length?true:false;
+        System.out.printf("Answer: %b%n",result);
+
+
 
     }
-}
+
+    }
+
